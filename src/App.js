2069789ed6f1268a27abc00159d7f8018eff6697
gegyhamdani <gegyhamdani@gmail.com>
2020-04-15 22:21:00 +0700

    create condition if there is no image

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ function App() {
     <div className="container mx-auto">
       <ImageSearch searchText={(text) => setTerm(text)} />
 
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
