a3385a3c7fd153a13ed45cdc6fedb7c9c389605d
gegyhamdani <gegyhamdani@gmail.com>
2020-06-10 22:28:25 +0700

    Updates

---
+++ precache-manifest.a9238c460f7645cc004eff5d0dd667f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f377a9c7ad1a83074b8c74e4a2d54a78",
+    "url": "/image-gallery/index.html"
+  },
+  {
+    "revision": "65d8b2774f9bad9d7dd4",
+    "url": "/image-gallery/static/css/main.40ec6352.chunk.css"
+  },
+  {
+    "revision": "6d6ae572b958af4fd95e",
+    "url": "/image-gallery/static/js/2.2cabef40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-gallery/static/js/2.2cabef40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d8b2774f9bad9d7dd4",
+    "url": "/image-gallery/static/js/main.fa3333f4.chunk.js"
+  },
+  {
+    "revision": "5406e722b5a0f9a705c3",
+    "url": "/image-gallery/static/js/runtime-main.23169a76.js"
+  }
+]);
